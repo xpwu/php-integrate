55d6754292fe5d18ef73650788dfcc434d0842d0
xpwu <xpwu@xpwu.local>
2016-11-17 01:06:08 +0800

    去掉不要的注释；fix bug

--- Main.php
+++ Main.php
@@ -14,8 +14,7 @@ class Main {
     $opt = getopt("hc:");
 
     $topDirStr = "integrate.conf.hphp";
-//    $integraterDir = '';
-//    $testPharName = '';
+
     if (!self::parseOpt($opt, $topDirStr)) {
       return;
     }
@@ -34,23 +33,11 @@ class Main {
       return;
     }
 
-//    if ($integraterDir !== '') {
-//      if (! $integrater->outputTo($integraterDir)) {
-//        return;
-//      }
-//    }
-
     if ($pharName !== '') {
       if (! $integrater->phar($pharName)) {
         return;
       }
     }
-
-//    if ($testPharName !== '') {
-//      if (! $integrater->pharForTest($testPharName)) {
-//        return;
-//      }
-//    }
 
     echo "---success---\n";
   }
@@ -59,40 +46,18 @@ class Main {
     static $usage = <<<EOF
 Usage:  option
   option: -h show this help;
-          -c conf name.
+          -c conf file name.
 EOF;
 
     if (array_key_exists('h', $opt) && $opt['h'] === false) {
       echo $usage.PHP_EOL;
       return false;
     }
-//    if (array_key_exists('m', $opt)) {
-//      if ($opt['m'] === 'dev') {
-//        $mode = Utils::DEV_MODE;
-//      } elseif ($opt['m'] === 'pro') {
-//        $mode = Utils::PRO_MODE;
-//      } else {
-//        echo $usage.PHP_EOL;
-//        return false;
-//      }
-//    }
+
     if (array_key_exists('c', $opt)) {
       $topDirStr = $opt['c'];
     }
-//    if (array_key_exists('i', $opt)) {
-//      $integraterDir = $opt['i'];
-//    }
-//    if (array_key_exists('p', $opt)) {
-//      $pharName = $opt['p'];
-//    }
-//    if (array_key_exists('s', $opt)) {
-//      $testPharName = $opt['s'];
-//    }
-//    if (!array_key_exists('i', $opt) && !array_key_exists('p', $opt)
-//        && !array_key_exists('s', $opt)) {
-//      echo $usage.PHP_EOL;
-//      return false;
-//    }
+
     return true;
   }
 
