048e276e6742c0d4264b53670753cec43be5f7ee
xpwu <xpwu@xpwu.local>
2016-11-17 01:06:41 +0800

    mkdir bin

--- phar-build.php
+++ phar-build.php
@@ -17,7 +17,28 @@ $files = [
 ];
 
 if (Phar::canWrite()) {
-  $ph = new Phar("pinte.phar");
+  // pinte
+  $ph = new Phar("bin/pinte_exe.phar");
+  $ph->startBuffering();
+  foreach ($files as $value) {
+    $ph[$value] = file_get_contents($value);
+  }
+  $ph->stopBuffering();
+  $stub = <<<EOF
+#!/usr/bin/env php
+<?php
+  Phar::interceptFileFuncs();
+  Phar::mapPhar();
+  set_include_path('phar://' . __FILE__ . PATH_SEPARATOR . get_include_path());
+  require_once('phar://' . __FILE__ ."/Main.php");
+  __HALT_COMPILER(); ?>
+EOF;
+  $ph->setStub($stub);
+  rename("bin/pinte_exe.phar", "bin/pinte");
+  chmod("bin/pinte", 777);
+
+  // pinte.phar
+  $ph = new Phar("bin/pinte.phar");
   $ph->startBuffering();
   foreach ($files as $value) {
     $ph[$value] = file_get_contents($value);
